ef15225dff93a573027a77760da2455acdb360a1
Rogatien de La Brunetière <rogatien.delabrunetire@gmail.com>
2023-05-16 16:36:38 +0200

    initialize project stucture: rest controller

---
+++ src/main/java/com/rogue/demo/rest/ImageRestController.java
@@ -0,0 +1,64 @@
+package com.rogue.demo.rest;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.rogue.demo.dao.ImageRepository;
+import com.rogue.demo.model.Image;
+import com.rogue.demo.model.ImageUploadResponse;
+import com.rogue.demo.util.ImageUtil;
+
+
+@RestController
+@CrossOrigin()
+@RequestMapping("/image")
+public class ImageRestController {
+
+	@Autowired
+	private ImageRepository imageRepository;
+
+	@PostMapping("/upload")
+	public ResponseEntity<ImageUploadResponse> uplaodImage(@RequestParam("image") MultipartFile file)
+			throws IOException {
+
+		final Image image = new Image();
+		image.setContent(ImageUtil.compressImage(file.getBytes()));
+		image.setName(file.getOriginalFilename());
+		image.setContentType(file.getContentType());
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(new ImageUploadResponse("Image uploaded successfully: " + file.getOriginalFilename()));
+	}
+
+	@GetMapping(path = { "/info/{name}" })
+	public Image getImageDetails(@PathVariable("name") String name) throws IOException {
+
+		final Optional<Image> dbImage = imageRepository.findByName(name);
+
+		final Image image = new Image();
+		image.setContent(ImageUtil.compressImage(dbImage.get().getContent()));
+		image.setName(dbImage.get().getName());
+		return image;
+	}
+
+	@GetMapping(path = { "/{name}" })
+	public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
+
+		final Optional<Image> dbImage = imageRepository.findByName(name);
+
+		return ResponseEntity.ok().contentType(MediaType.valueOf(dbImage.get().getContentType()))
+				.body(ImageUtil.decompressImage(dbImage.get().getContent()));
+	}
+}
