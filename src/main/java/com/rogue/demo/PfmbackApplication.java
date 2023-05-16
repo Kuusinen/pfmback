03d623452a55b8e62392752c8bf3713fa1eaeef1
Rogatien de La Brunetière <rogatien.delabrunetire@gmail.com>
2023-05-16 14:36:33 +0200

    initialize Sping boot project

---
+++ src/main/java/com/rogue/demo/PfmbackApplication.java
@@ -0,0 +1,13 @@
+package com.rogue.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogue/demo/PfmbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogue.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
